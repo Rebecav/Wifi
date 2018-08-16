1edc969d3cb1bbe30ab2dff75d8cab9c24ab9ac8
Rebecav <rebecav@tywls-astoria.org>
2018-08-16 14:02:25 -0400

    Importing JSON files

--- Wifi.py
+++ Wifi.py
@@ -4,5 +4,11 @@ wifiFile = open("wifidata.json" , "r")
 wifiData =json.load(wifiFile)
 wifiFile.close()
 
+wifiFile2 = open("HotSpotdata2.json", "r")
+wifiData2 = json.load(wifiFile2)
+wifiFile2.close()
+
 for booth in wifiData["data"]:
     print(booth[10])
+for booth2 in wifiData2["data"]:
+    print(booth2[10])
