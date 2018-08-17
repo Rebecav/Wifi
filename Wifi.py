4b4bdb56d7b7c798a0c161634b4017ec61425c64
lthain <42009917+lthain@users.noreply.github.com>
2018-08-17 13:41:29 -0400

    Updated so add. are printed

--- Wifi.py
+++ Wifi.py
@@ -1,14 +1,8 @@
 import json
 
-wifiFile = open("wifidata.json" , "r")
-wifiData =json.load(wifiFile)
-wifiFile.close()
-
 wifiFile2 = open("HotSpotdata2.json", "r")
 wifiData2 = json.load(wifiFile2)
 wifiFile2.close()
 
-for booth in wifiData["data"]:
-    print(booth[10])
 for booth2 in wifiData2["data"]:
-    print(booth2[10])
+    print(booth2[16])
